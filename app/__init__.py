06b5e2efc5db5bf2ee031489e3cc0034fd08163b
haolinl4 <haolinl4@illinois.edu>
2021-07-22 16:39:41 -0500

    Add query logic for food search. Confirm that the code can connect to the gcp mysql server.

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,12 @@
 import os
 import sqlalchemy
 from yaml import load, Loader
-from flask import Flask, render_template
+from flask import Flask, render_template, request, Response
 
 app = Flask(__name__)
 
 
-def init_connect_engine():
+def init_connect_engine() -> sqlalchemy.engine.Engine:
 	if os.environ.get('GAE_ENV') != 'standard':
 		# GAE_ENV appears on GCP
 		# we are on a local machine here
@@ -16,21 +16,58 @@ def init_connect_engine():
 		for var in env_variables:
 			os.environ[var] = env_variables[var]
 
+	db_config = {
+        # [START cloud_sql_mysql_sqlalchemy_limit]
+        # Pool size is the maximum number of permanent connections to keep.
+        "pool_size": 5,
+        # Temporarily exceeds the set pool_size if no connections are available.
+        "max_overflow": 2,
+        # The total number of concurrent connections for your application will be
+        # a total of pool_size and max_overflow.
+        # [END cloud_sql_mysql_sqlalchemy_limit]
+
+        # [START cloud_sql_mysql_sqlalchemy_backoff]
+        # SQLAlchemy automatically uses delays between failed connection attempts,
+        # but provides no arguments for configuration.
+        # [END cloud_sql_mysql_sqlalchemy_backoff]
+
+        # [START cloud_sql_mysql_sqlalchemy_timeout]
+        # 'pool_timeout' is the maximum number of seconds to wait when retrieving a
+        # new connection from the pool. After the specified amount of time, an
+        # exception will be thrown.
+        "pool_timeout": 30,  # 30 seconds
+        # [END cloud_sql_mysql_sqlalchemy_timeout]
+
+        # [START cloud_sql_mysql_sqlalchemy_lifetime]
+        # 'pool_recycle' is the maximum number of seconds a connection can persist.
+        # Connections that live longer than the specified amount of time will be
+        # reestablished
+        "pool_recycle": 1800,  # 30 minutes
+        # [END cloud_sql_mysql_sqlalchemy_lifetime]
+
+    }
+
 	pool = sqlalchemy.create_engine(
-						sqlalchemy.engine.url.URL(
-							drivername='mysql+pymysql',
-							username=os.environ.get('MYSQL_USER'), 			# user name
-							password=os.environ.get('MYSQL_PASSWORD'), 	# user password
-							database=os.environ.get('MTSQL_DB'), 				# db name
-							host=os.environ.get('MYSQL_HOST') 					#ip
-						)
+						# mysql+pymysql://<db_user>:<db_pass>@<db_host>:<db_port>/<db_name>
+						# sqlalchemy.engine.url.URL(
+						# 	drivername='mysql+pymysql',
+						# 	username=os.environ.get('MYSQL_USER'), 			# user name
+						# 	password=os.environ.get('MYSQL_PASSWORD'), 	# user password
+						# 	database=os.environ.get('MTSQL_DB'), 				# db name
+						# 	host=os.environ.get('MYSQL_HOST') 					#ip
+						# ),
+						'mysql+pymysql://root:@34.135.179.50:3306/calometer',
+
+						**db_config
 	)
 
 	return pool
 
 db = init_connect_engine()
 
-connection = db.connect()
-# connection.execute()
+# connection = db.connect()
+# results = connection.execute('select * from Food limit 5;').fetchall()
+# print([x for x in results])
+# connection.close()
 
 from app import routes
--- app/database.py
+++ app/database.py
@@ -1,27 +1,82 @@
-def get_food_search_result(food_name: str, ) -> dict:
+from app import db
+
+def get_food_search_result(food_name: str, 
+	protein_low: float, protein_high: float,
+	calorie_low: float, calorie_high: float,
+	fat_low: float, fat_high: float,
+	carbo_low: float, carbo_high: float) -> list:
 	'''Make the database query with the given parameter'''
 
-	# currently in 
-	search_result_list = [
-		{
-			'name':'ultimate food',
-			'protein':1000,
-			'calorie':200,
-			'fat':0.01,
-			'carbohydrate':123,
-			'serving size':100
-		},
-		{
-			'name':'ultimate food 2',
-			'protein':2000,
-			'calorie':400,
-			'fat':0.02,
-			'carbohydrate':246,
-			'serving size':200
-		}
-	]
+	fields = '.fdc_id, f.Description, fn.nutrient_id, fn.Amount'
+	tables = 'Food f JOIN Food_nutrient fn ON f.fdc_id = fn.fdc_id '
+	condition = ''
+	if food_name != '':
+		condition += 'f.Description LIKE \'%' + food_name + '%\''
 
-	return search_result_list
+	if protein_low != -1 and protein_high != -1:
+		condition += ' AND (fn.nutrient_id = 1003 AND fn.Amount >= ' + str(protein_low) + ' AND fn.Amount <= ' + str(protein_high) + ')'
+	elif protein_low == -1 and protein_high != -1:
+		condition += ' AND (fn.nutrient_id = 1003 AND fn.Amount <= ' + str(protein_high) + ')'
+	elif protein_low != -1 and protein_high == -1:
+		condition += ' AND (fn.nutrient_id = 1003 AND fn.Amount >= ' + str(protein_low) + ')'
+
+	if calorie_low != -1 and calorie_high != -1:
+		condition += ' AND (fn.nutrient_id = 1008 AND fn.Amount >= ' + str(calorie_low) + ' AND fn.Amount <= ' + str(calorie_high) + ')'
+	elif calorie_low == -1 and calorie_high != -1:
+		condition += ' AND (fn.nutrient_id = 1008 AND fn.Amount <= ' + str(calorie_high) + ')'
+	elif calorie_low != -1 and calorie_high == -1:
+		condition += ' AND (fn.nutrient_id = 1008 AND fn.Amount >= ' + str(calorie_low) + ')'
+
+	if fat_low != -1 and fat_high != -1:
+		condition += ' AND (fn.nutrient_id = 1085 AND fn.Amount >= ' + str(fat_low) + ' AND fn.Amount <= ' + str(fat_high) + ')'
+	elif fat_low == -1 and fat_high != -1:
+		condition += ' AND (fn.nutrient_id = 1085 AND fn.Amount <= ' + str(fat_high) + ')'
+	elif fat_low != -1 and fat_high == -1:
+		condition += ' AND (fn.nutrient_id = 1085 AND fn.Amount >= ' + str(fat_low) + ')'
+
+	if carbo_low != -1 and carbo_high != -1:
+		condition += ' AND (fn.nutrient_id = 2039 AND fn.Amount >= ' + str(carbo_low) + ' AND fn.Amount <= ' + str(carbo_high) + ')'
+	elif carbo_low == -1 and carbo_high != -1:
+		condition += ' AND (fn.nutrient_id = 2039 AND fn.Amount <= ' + str(carbo_high) + ')'
+	elif carbo_low != -1 and carbo_high == -1:
+		condition += ' AND (fn.nutrient_id = 2039 AND fn.Amount >= ' + str(carbo_low) + ')'
+
+	order = 'f.fdc_id'
+
+	query = ('SELECT {fields} '
+				 'FROM {tables} '
+				 'WHERE {condition} '
+				 'ORDER BY {order};')
+
+	connection = db.connect()
+	query_results = connection.execute(query).fetchall()
+
+	food_list = []
+
+	fn_item = dict()
+	current_fdc_id = -1
+	for result_tuple in query_results:
+		if current_fdc_id == -1:
+			fn_item['food name'] = result_tuple[1]
+			current_fdc_id = result_tuple[0]
+		elif current_fdc_id != result_tuple[0]:
+			current_fdc_id = result_tuple[0]
+			food_list.append(fn_item)
+
+			fn_item.clear()
+
+			fn_item['food name'] = result_tuple[1]
+
+		if result_tuple[3] == 1003:
+			fn_item['protein'] = result_tuple[4]
+		elif result_tuple[3] == 1008:
+			fn_item['calories'] = result_tuple[4]
+		elif result_tuple[3] == 1085:
+			fn_item['fat'] = result_tuple[4]
+		elif result_tuple[3] == 2039:
+			fn_item['carbohydrate'] = result_tuple[4]
+
+	return food_list
 
 def register_user(register_email: str, user_name: str, password: str, 
 	first_name: str, last_name: str, date_of_birth: str, sex: int) -> None:
@@ -33,7 +88,7 @@ def update_user(user_id: int) -> None:
 def delete_user(user_id: int) -> None:
 	pass
 
-def get_user_goals(user_id: int) -> dict:
+def get_user_goals(user_id: int) -> None:
 	pass  
 
 def add_user_goal(current_weight: float, goal_weight: float) -> None:
