dc04efa9f2c4b439be84d29ba5155808a1fe77c4
stellazhangx <cenz2@illinois.edu>
2021-07-23 03:45:46 -0500

    update

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 from app import app
-from flask import render_template, request, Response
+from flask import render_template, request, Response, redirect, url_for
 from app import database as db_helper
 
 
@@ -32,11 +32,32 @@ def homepage():
 
 @app.route('/register', methods=['POST'])
 def register():
-	return render_template('test_template.html', name='cyka blyat')
+	if request.method == 'POST':
+		submitted_search_parameters = request.get_json()
+
+		email = submitted_search_parameters['email']
+		user_name = submitted_search_parameters['user_name']
+		password = submitted_search_parameters['pwd']
+		first_name = submitted_search_parameters['f_name']
+		last_name = submitted_search_parameters['l_name']
+		date_of_birth = submitted_search_parameters['dob']
+		sex = submitted_search_parameters['sex']
+		
+		return redirect(url_for("add_goals"))
+	else:
+		return render_template('test_template.html', name='cyka blyat')
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-	return render_template('test_template.html', name='cyka blyat')
+	if request.method == 'POST':
+		submitted_search_parameters = request.get_json()
+
+		email = submitted_search_parameters['email']
+		password = submitted_search_parameters['pwd']
+
+		return redirect(url_for("daily_entry"))
+	else:
+		return render_template('test_template.html', name='cyka blyat')
 
 @app.route('/goals', methods=['GET', 'POST'])
 def goals():
