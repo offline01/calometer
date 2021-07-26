fe11c373f2e6525caa6e033f1c0cf6ec00f311fd
offline01 <zhuangxy12@gmail.com>
2021-07-25 19:55:01 -0700

    delete redundant files

--- src/components/pages/mf/ResetPassword.js
+++ src/components/pages/mf/ResetPassword.js
@@ -2,11 +2,10 @@ import React, { useState } from "react";
 import { Auth } from "aws-amplify";
 import { Link } from "react-router-dom";
 import {
-  HelpBlock,
+  FormText,
   FormGroup,
-  Glyphicon,
   FormControl,
-  ControlLabel,
+  FormLabel,
 } from "react-bootstrap";
 import LoaderButton from "../components/LoaderButton";
 import { useFormFields } from "../libs/hooksLib";
@@ -73,7 +72,7 @@ export default function ResetPassword() {
     return (
       <form onSubmit={handleSendCodeClick}>
         <FormGroup bsSize="large" controlId="email">
-          <ControlLabel>Email</ControlLabel>
+          <FormLabel>Email</FormLabel>
           <FormControl
             autoFocus
             type="email"
@@ -98,20 +97,20 @@ export default function ResetPassword() {
     return (
       <form onSubmit={handleConfirmClick}>
         <FormGroup bsSize="large" controlId="code">
-          <ControlLabel>Confirmation Code</ControlLabel>
+          <FormLabel>Confirmation Code</FormLabel>
           <FormControl
             autoFocus
             type="tel"
             value={fields.code}
             onChange={handleFieldChange}
           />
-          <HelpBlock>
+          <FormText>
             Please check your email ({fields.email}) for the confirmation code.
-          </HelpBlock>
+          </FormText>
         </FormGroup>
         <hr />
         <FormGroup bsSize="large" controlId="password">
-          <ControlLabel>New Password</ControlLabel>
+          <FormLabel>New Password</FormLabel>
           <FormControl
             type="password"
             value={fields.password}
@@ -119,7 +118,7 @@ export default function ResetPassword() {
           />
         </FormGroup>
         <FormGroup bsSize="large" controlId="confirmPassword">
-          <ControlLabel>Confirm Password</ControlLabel>
+          <FormLabel>Confirm Password</FormLabel>
           <FormControl
             type="password"
             value={fields.confirmPassword}
@@ -142,7 +141,6 @@ export default function ResetPassword() {
   function renderSuccessMessage() {
     return (
       <div className="success">
-        <Glyphicon glyph="ok" />
         <p>Password has been reset.</p>
         <p>
           <Link to="/login">
